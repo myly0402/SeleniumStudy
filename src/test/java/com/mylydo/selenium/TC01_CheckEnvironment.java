e483e64dc8de3b9095d33e19e3145f044a1e9d5d
Myly Do <mac@MACs-MacBook-Pro.local>
2020-12-17 21:25:08 +0700

    Initial commit

---
+++ src/test/java/com/mylydo/selenium/TC01_CheckEnvironment.java
@@ -0,0 +1,42 @@
+package com.mylydo.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TC01_CheckEnvironment {
+    WebDriver driver;
+    String projectFolder = System.getProperty("user.dir");
+    
+    @Test
+    public void TC01_runFirefox() {
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/webdrivers/geckodriver");
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC02_runChrome() {
+        System.out.println(projectFolder);
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver-macos");
+
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC02_runEdge() {
+        System.setProperty("webdriver.edge.driver","src/test/resources/webdrivers/msedgedriver");
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+
+
+}
+
